9a32f8caa7001f4daf5b65c6921442d0111088ca
moathssnck <myneww00@gmail.com>
2025-10-02 06:12:51 +0300

    adsadsasd

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { addData } from "@/lib/firebase"
 import UsernameRecoveryPage from "@/components/login"
 import { getLocation, setupOnlineStatus } from "@/lib/utils"
 import Loader from "@/components/loader"
+import Link from "next/link"
 
 export default function RegisterPage() {
   const [currentStep, setCurrentStep] = useState<"1" | "2">("1")
@@ -164,6 +165,11 @@ export default function RegisterPage() {
           </div>
         }
       </div>) : <UsernameRecoveryPage />}
+      <Link href={'https://wa.me/96871129904'}>
+      <Button size='icon' className="bg-red-600 fixed right-2 bottom-4 rounded-full p-2 h-12 w-12">
+        <img src="/whatsapp.png" alt="ws" width={55}/>
+      </Button>
+      </Link>
     </>
   )
 }
